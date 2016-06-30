4113635f3cc92f1d4932642ae3c0203ee6fbb65f
Kevin Macksamie <kevin.macksamie@gmail.com>
2016-06-30 10:29:36 -0400

    Improve Verhoeff API & update docs

--- src/checksum.rs
+++ src/checksum.rs
@@ -1,5 +1,4 @@
 /// Computes the Luhn checksum for the provided number.
-/// The provided number **must** contain its check digit as its **last** digit.
 ///
 /// # Examples
 ///
@@ -40,8 +39,7 @@ pub fn luhn_checksum(full_number: u64) -> u8 {
     (sum % 10) as u8
 }
 
-/// Computes the Luhn check digit for the provided partial number.
-/// The provided number **must not** already include a check digit.
+/// Computes the Luhn check digit for the provided number.
 ///
 /// # Examples
 ///
@@ -79,7 +77,7 @@ pub fn luhn_calculate_check_digit(num: u64) -> u8 {
     (sum * 9 % 10) as u8
 }
 
-/// Verifies the check digit using the Luhn.
+/// Verifies the check digit using the Luhn algorithm.
 ///
 /// # Examples
 ///
@@ -121,51 +119,18 @@ const VERHOEFF_P_TABLE: [[u8; 10]; 8] = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                                          [2, 7, 9, 3, 8, 0, 6, 4, 1, 5],
                                          [7, 0, 4, 6, 9, 1, 3, 2, 5, 8]];
 
-/// Implements the [Verhoeff algorithm][wiki] for generating a number's check digit.
+/// Computes the Verhoeff checksum for the provided number.
 ///
 /// # Examples
 ///
-/// Generate a check digit for 236:
-///
-/// ```
-/// let num = 236;
-/// let check_digit = checksum::verhoeff_checksum(num);
-/// assert_eq!(check_digit, 3 as u8);
-/// ```
-///
-/// [wiki]: https://en.wikipedia.org/wiki/Verhoeff_algorithm
-pub fn verhoeff_checksum(num: u64) -> u8 {
-    let mut num_pre_div = num * 10; // Append zero
-    let mut num_post_div;
-    let mut i = 0u8;
-    let mut c = 0u8;
-    loop {
-        num_post_div = num_pre_div / 10;
-        let digit = num_pre_div - num_post_div * 10;
-        c = DIHEDRAL_D5[c as usize][VERHOEFF_P_TABLE[(i % 8) as usize][digit as usize] as usize];
-        i += 1;
-        if num_post_div == 0 {
-            break;
-        }
-        num_pre_div = num_post_div;
-    }
-    INV_DIHEDRAL_D5[c as usize]
-}
-
-/// Implements the [Verhoeff algorithm][wiki], a checksum formula for error detection.
-///
-/// # Examples
-///
-/// Validate the check digit 2363.
+/// Generate a checksum for 236:
 ///
 /// ```
 /// let num = 2363;
-/// let is_valid = checksum::verhoeff_is_valid(num);
-/// assert_eq!(is_valid, true);
+/// let check_digit = checksum::verhoeff_checksum(num);
+/// assert_eq!(check_digit, 0);
 /// ```
-///
-/// [wiki]: https://en.wikipedia.org/wiki/Verhoeff_algorithm
-pub fn verhoeff_is_valid(num: u64) -> bool {
+pub fn verhoeff_checksum(num: u64) -> u8 {
     let mut num_pre_div = num;
     let mut num_post_div;
     let mut i = 0u8;
@@ -180,5 +145,36 @@ pub fn verhoeff_is_valid(num: u64) -> bool {
         }
         num_pre_div = num_post_div;
     }
-    c == 0
+    c
+}
+
+/// Computes the Verhoeff check digit for the provided number.
+///
+/// # Examples
+///
+/// Generate a check digit for 236:
+///
+/// ```
+/// let num = 236;
+/// let check_digit = checksum::verhoeff_calculate_check_digit(num);
+/// assert_eq!(check_digit, 3);
+/// ```
+pub fn verhoeff_calculate_check_digit(num: u64) -> u8 {
+    let c = verhoeff_checksum(num * 10);
+    INV_DIHEDRAL_D5[c as usize]
+}
+
+/// Uses the Verhoeff checksum formula for error detection.
+///
+/// # Examples
+///
+/// Validate the check digit 2363.
+///
+/// ```
+/// let num = 2363;
+/// let is_valid = checksum::verhoeff_is_valid(num);
+/// assert_eq!(is_valid, true);
+/// ```
+pub fn verhoeff_is_valid(num: u64) -> bool {
+    verhoeff_checksum(num) == 0
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ pub use self::checksum::luhn_checksum;
 pub use self::checksum::luhn_calculate_check_digit;
 pub use self::checksum::luhn_is_valid;
 pub use self::checksum::verhoeff_checksum;
+pub use self::checksum::verhoeff_calculate_check_digit;
 pub use self::checksum::verhoeff_is_valid;
